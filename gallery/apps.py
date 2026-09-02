395da7b8105056f2f7f5885d7e90a9d4d920f24d
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 00:04:23 +0300

    feat: update GalleryConfig class structure and ensure signals are imported

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,4 +1,13 @@
 from django.apps import AppConfig
+from django.apps import AppConfig
+
+
+class GalleryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gallery'
+
+    def ready(self):
+        import gallery.signals
 
 
 class GalleryConfig(AppConfig):

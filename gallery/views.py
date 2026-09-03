e890aeb1f9a6ab23fee205dbc9e3a696cf3b9929
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 21:40:57 +0300

    feat: refactor URL configuration and enhance register view functionality

--- gallery/views.py
+++ gallery/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
-from .forms import ProfileForm
+from .forms import RegisterForm, ProfileForm
+
+
+def home(request):
+    return render(request, "gallery/home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = RegisterForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {"form": form}
+    )
 
 
 @login_required
 def profile(request):
-
     profile = request.user.profile
 
     if request.method == "POST":
-
         form = ProfileForm(
             request.POST,
             request.FILES,
@@ -30,22 +49,4 @@ def profile(request):
             "form": form,
             "profile": profile
         }
-    )
-
-
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-
-    else:
-        form = RegisterForm()
-
-    return render(
-        request,
-        "registration/register.html",
-        {"form": form}
     )

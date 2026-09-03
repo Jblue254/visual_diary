2704e022cda5686b42d6cfbfe0ab3c6660796a06
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 21:36:54 +0300

    feat: add media URL handling in development and update settings structure

--- config/urls.py
+++ config/urls.py
@@ -27,3 +27,8 @@ urlpatterns = [
         include("django.contrib.auth.urls")
     ),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )

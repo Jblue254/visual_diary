9a5d9828a132ee37944697a611ebb405afaeb801
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 22:06:16 +0300

    feat: add Photo model migration and implement photo detail view with filtering

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, ProfileForm
+from .models import Photo
 
 
 def home(request):
-    return render(request, "gallery/home.html")
+    tag = request.GET.get("tag")
+
+    photos = Photo.objects.all().order_by("-created_at")
+
+    if tag:
+        photos = photos.filter(tags__icontains=tag)
+
+    return render(
+        request,
+        "gallery/home.html",
+        {"photos": photos}
+    )
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(
+        Photo,
+        pk=pk
+    )
+
+    return render(
+        request,
+        "gallery/photo_detail.html",
+        {"photo": photo}
+    )
 
 
 def register(request):

c04b71b0d600f823667ccb3363594e2702d75aed
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 21:46:04 +0300

    feat: register Photo model in admin interface

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Photo, Profile
 
 admin.site.register(Profile)
+admin.site.register(Photo)
 
 # Register your models here.

8b32eb6814a3f49d630d6bd1d6bf4b76f9ff6047
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-03 22:07:07 +0300

    feat: add photo detail URL pattern to urlpatterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+    path(
+        "photo/<int:pk>/",
+        views.photo_detail,
+        name="photo_detail"
+    ),
 ]
